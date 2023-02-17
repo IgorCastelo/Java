e9e24a59b17cb8b3cf07c63c9e30c2be46c2d707
Igor Castelo <freitascasteloigor@gmail.com>
2023-02-16 23:33:09 -0300

    Foram apresentadas as intruções 'If' e 'Switch'. Para cada instrução foi escrito um programa de exemplo.

---
+++ Instrucoes/src/main/java/Instrucoes/Main.java
@@ -0,0 +1,87 @@
+
+package Instrucoes;
+      
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+         
+         //Instrução if
+       
+         /* int a = 8;    
+         
+         if(a==9){
+        System.out.println("a=9");
+        System.out.println("Você acertou!");
+        }
+        else System.out.println("a<>9");
+         */
+         
+        
+         //if identado
+         
+        /* int b =15 
+         
+         if(b==8)
+         System.out.println("b=8");
+         else
+             if(b<9)
+                 System.out.println("b<9");
+             else System.out.println("b>9");
+         */
+         
+       
+       //Exercício if
+       /*        
+       Scanner input = new Scanner(System.in);
+       
+       double nota1, nota2, nota3, media;
+       
+       
+       System.out.println("Digite sua primeira nota:");
+       nota1=(input.nextDouble());
+       
+       System.out.println("Digite sua segunda nota:");
+       nota2=(input.nextDouble());
+       
+       System.out.println("Digite sua terceira nota:");
+       nota3=(input.nextDouble());
+       
+       media = (nota1+nota2+nota3)/3;
+       
+       if(media<7)
+           System.out.println("Voce ficou de recuperacao ;/");
+       else
+           if(media==10)
+               System.out.println("Voce foi aprovado com honrra! Parabens!");
+       else System.out.println("Voce foi aprovado.");
+       */
+       
+       
+
+//Istrução Switch(exemplo de validação de termos do usuário).
+     /*  char resposta = 's';
+       
+     
+       switch(resposta){
+           case 'S':
+           case 's':    
+               System.out.println("Voce aceitou os termos");
+               break;
+           case 'N':
+           case 'n':    
+               System.out.println("voce nao ceitou os termos");
+               break;
+           default:
+                System.out.println("Error");
+                
+       }
+   */ 
+       
+       
+       
+               
+    }
+    
+}
