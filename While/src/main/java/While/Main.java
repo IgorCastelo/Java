03051a1c9765db277aa6d35399b823991b831437
Igor Castelo <freitascasteloigor@gmail.com>
2023-02-23 12:46:01 -0300

    Foi criado o arquivo com o exercício sobre o laço For.

---
+++ While/src/main/java/While/Main.java
@@ -0,0 +1,26 @@
+
+package While;
+
+public class Main {
+
+    //Whille
+    public static void main(String[] args) {
+      int i = 1;
+      int max = 30;
+      
+      /*  while(i<max){
+      System.out.println("O valor de i = " + i);
+      i++;
+     
+      }*/
+      //do while
+        do{
+            i++;
+            System.out.println("O valor de i = " + i );
+            }
+            while(i<max);
+        }    
+      
+    }
+    
+
