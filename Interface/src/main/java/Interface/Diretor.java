4074ce64ef727e6c2c652d7fad4e4704d0c041d1
Igor Castelo <freitascasteloigor@gmail.com>
2023-02-23 13:02:02 -0300

    Exercício sobre Interface

---
+++ Interface/src/main/java/Interface/Diretor.java
@@ -0,0 +1,9 @@
+
+package Interface;
+
+public class Diretor implements Autenticacao {
+    public void autenticar(){
+    System.out.println("Aqui desenvolvo  como o diretor vai se autenticar.");
+    }
+    
+}
---
+++ Interface/src/main/java/Interface/PessoaJuridica.java
@@ -0,0 +1,8 @@
+
+package Interface;
+
+public class PessoaJuridica implements Autenticacao {
+    public void autenticar(){
+    System.out.println("Aqui desenvolvo acomo a pessoa juridica irá se autentica.");
+    }
+}
