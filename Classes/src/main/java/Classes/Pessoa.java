3cd84134989416adcbc8b326c7f61967135e880d
Igor Castelo <freitascasteloigor@gmail.com>
2023-02-23 12:57:09 -0300

    Exercício sobre Classes

---
+++ Classes/src/main/java/Classes/Pessoa.java
@@ -0,0 +1,37 @@
+
+package Classes;
+
+public class Pessoa {
+  //Atributos
+    private float peso;
+    private float altura;
+
+        //Métodos Acessores
+    
+    public Pessoa(float peso, float altura){ 
+    this.peso = peso;
+    this.altura = altura;
+        
+    }
+    public float calcularIMC(){
+        float imc = peso/(altura*altura);
+        return imc;
+    }
+ 
+    
+    public void setPeso (float peso){
+        this.peso = peso;
+        
+    }
+    public float getPeso(){
+        return peso;
+    }
+    public void setAltura(float altura){
+        this.altura = altura;
+        
+    }   
+    public float getAltura(){
+        return altura;
+    }
+}
+
