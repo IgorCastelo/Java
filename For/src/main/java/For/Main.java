94bf6a48553bab86210e33ffc0dec56564d5eb9b
Igor Castelo <freitascasteloigor@gmail.com>
2023-02-23 12:41:46 -0300

    Foi criado o arquivo com o exercicio sobre a aula

---
+++ For/src/main/java/For/Main.java
@@ -0,0 +1,14 @@
+
+package For;
+
+public class Main {
+
+  
+    public static void main(String[] args) {
+  
+        for(int i=1; i<=5;i++){// Inicializei a variavel i com 1 e adicionei igualdade à condição para puder fazer o o sistema mpstra de 1 ate 5.
+        System.out.println("'i' e igual a ="+i);
+        }
+    }
+    
+}
