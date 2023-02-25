79978b440da6d111783c7859161847a282b3f553
Igor Castelo <freitascasteloigor@gmail.com>
2023-02-25 00:12:35 -0300

    Nesse arquivo contem a atividade sobre a aula de sobre-carga de métodos e construtores.

---
+++ SobreCargaDeMetodos/src/main/java/SobreCargaDeMetodos/TesteCalculadora.java
@@ -0,0 +1,14 @@
+package SobreCargaDeMetodos;
+
+public class TesteCalculadora {
+    
+    public static void main(String[] args){
+        MinhaCalculadora calc = new MinhaCalculadora();
+        
+        System.out.println(calc.soma(1,2,3));
+        System.out.println(calc.soma(3.0,3.0));
+        
+    
+    }
+
+}
