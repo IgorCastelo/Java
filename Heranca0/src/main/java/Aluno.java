86a1faee53283e56afc13bc377cd9d73a293f1fa
Igor Castelo <freitascasteloigor@gmail.com>
2023-03-01 13:31:44 -0300

    Foi explicado o conceito de Herança entre classes e de Super Classe através de um exercício prático

---
+++ Heranca0/src/main/java/Aluno.java
@@ -0,0 +1,30 @@
+
+public class Aluno extends Pessoa{
+   
+    private String[] cursos;
+    private String[][] notas;
+    
+    Aluno(){}
+
+    public String[] getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(String[] cursos) {
+        this.cursos = cursos;
+    }
+
+    public String[][] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(String[][] notas) {
+        this.notas = notas;
+    }
+    public double calcularMedia(double num1, double num2, double num3){
+    double media = (num1+num2+num3/3);
+    return media;
+    }
+    
+    }
+
---
+++ Heranca0/src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    
+    public static void main(String[] args){
+        Aluno aluno = new Aluno();
+        
+        aluno.setNome("Igor");
+        System.out.println(aluno.getNome());
+        
+    }
+    
+}
